44e979fe409dba7a1177bc0ba0f2f1845d518753
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-10-13 17:20:04 -0300

    Initial structure - part 2

--- ft_printf.c
+++ ft_printf.c
@@ -1,38 +1,53 @@
 #include "ft_printf.h"
 
-int	print_type(const char *format, va_list args);
-
-int	ft_printf(const char *format, ...)
+int	print_str(char *str)
 {
-	va_list	args;
-	int	char_count;
-	char	*percent;
-	char	*aux;
-	size_t	aux_len;
-
-	if (!format)
-		return (0);
-	va_start(args, format);
-	char_count = 0;
-	while (*format)
-	{
-		percent = ft_strchr(format, '%');
-		aux = ft_substr(format, 0, percent - format);
-		ft_putstr_fd(aux, STDOUT_FILENO);
-		aux_len = ft_strlen(aux);
-		char_count += aux_len;
-		format += aux_len;
-		if (percent)
-			char_count += print_type(++format, args);
-		free(aux);
-	}
-	va_end(args);
-	return (char_count);
+	ft_putstr_fd(str, STDOUT_FILENO);
+	return (ft_strlen(str));
 }
 
-int	print_type(const char *format, va_list args)
+char	*get_format_specifier(const char *format)
 {
-	switch(*format)
+	return ft_strchr(format, '%');
+}
+
+int	print_without_format(const char **format, char *format_specifier,
+			int *char_count)
+{
+	char	*str_without_format;
+	size_t	len;
+
+	len = format_specifier - *format;
+	if (!format_specifier)
+		len = ft_strlen(*format);
+	str_without_format = ft_substr(*format, 0, len);
+	if (!str_without_format)
+		return (-1);
+	*char_count += print_str(str_without_format);
+	*format += len;
+	free(str_without_format);
+	return (0);
+}
+
+int	check_flags(const char **format, size_t flags[])
+{
+	while (**format
+	&& (ft_strchr("-0.# +", **format)
+	|| ft_isdigit(**format)))
+		(*format)++;
+	return (0);
+}
+
+#define NUMBER_OF_FLAGS 7
+
+int	print_formated(const char **format, va_list args, int *char_count)
+{
+	size_t	flags[NUMBER_OF_FLAGS];
+
+	(*format)++;
+	if (check_flags(format, flags))
+		return (-1);
+	switch(**format)
 	{
 		case 'c':
 		case 's':
@@ -46,4 +61,28 @@ int	print_type(const char *format, va_list args)
 			return (0);
 	}
 	va_arg(args, int);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int	char_count;
+	char	*format_specifier;
+
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	char_count = 0;
+	while (*format)
+	{
+		format_specifier = get_format_specifier(format);
+		if (print_without_format(&format, format_specifier,
+				&char_count))
+			return (-1);
+		if (format_specifier
+		&& print_formated(&format, args, &char_count))
+			return (-1);
+	}
+	va_end(args);
+	return (char_count);
 }
