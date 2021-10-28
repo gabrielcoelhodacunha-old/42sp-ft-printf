48fcc5b6e8d44bd7aed98fbec7e4875e02af8a01
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-10-28 09:59:23 -0300

    print_char using print_string

--- src/handle_flags.c
+++ src/handle_flags.c
@@ -4,11 +4,5 @@ void	handle_flags(char **str, int *flags, char format)
 {
 	if (!flags)
 		return ;
-	handle_zero_padding(str, flags);
 	handle_alternate_form(str, flags, format);
-	/*
-	handle_negative_width(str, flags, width);
-	handle_blank(str, flags);
-	handle_signed(str, flags);
-	*/
 }
--- src/print_char.c
+++ src/print_char.c
@@ -2,7 +2,23 @@
 
 int	print_char(char c, int *flags)
 {
-	(void) flags;
-	ft_putchar_fd(c, STDOUT_FILENO);
-	return (1);
+	char	str[2];
+	int	len;
+
+	str[0] = c;
+	str[1] = '\0';
+	len = 1;
+	if (!c && flags[NEGATIVE_WIDTH])
+	{
+		ft_putchar_fd(c, STDOUT_FILENO);
+		len += print_string(str, flags, 'c');
+	}
+	else if (!c)
+	{
+		len += print_string(str, flags, 'c');
+		ft_putchar_fd(c, STDOUT_FILENO);
+	}
+	else
+		len = print_string(str, flags, 'c');
+	return (len);
 }
