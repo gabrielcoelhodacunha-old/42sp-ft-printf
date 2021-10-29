5c312bcbd59d3a1e8a31ce4fc7d1fc67c0cc9c4a
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-10-29 14:56:41 -0300

    Refactored handle_width

--- src/handle_width.c
+++ src/handle_width.c
@@ -6,46 +6,59 @@
 /*   By: gcoelho- <gcoelho-@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 10:14:01 by gcoelho-          #+#    #+#             */
-/*   Updated: 2021/10/29 14:32:41 by gcoelho-         ###   ########.fr       */
+/*   Updated: 2021/10/29 14:55:24 by gcoelho-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static size_t	get_str_len(char **str, size_t *flags, char format);
+static size_t	get_str_len(char *str, size_t *flags, char format);
 static char		*get_padding(size_t padding_len, size_t *flags);
+static int		check_if_is_negative(char *str, size_t *flags, char format);
+static char		*get_padded_str(char **old_str, char *padding,
+					int is_negative, size_t *flags);
 
 void	handle_width(char **str, size_t *flags, char format)
 {
 	char	*old_str;
 	char	*padding;
-	size_t	padding_len;
 	size_t	str_len;
 	int		is_negative;
 
-	str_len = get_str_len(str, flags, format);
+	str_len = get_str_len(*str, flags, format);
 	if (flags[FIELD_WIDTH] <= str_len)
 		return ;
 	old_str = *str;
-	padding_len = flags[FIELD_WIDTH] - str_len;
-	padding = get_padding(padding_len, flags);
-	if (ft_strchr("di", format) && (*str, '-')
-		&& flags[ZERO_PADDING])
-		is_negative = 1;
-	else
-		is_negative = 0;
+	padding = get_padding(flags[FIELD_WIDTH] - str_len, flags);
+	is_negative = check_if_is_negative(*str, flags, format);
+	*str = get_padded_str(&old_str, padding, is_negative, flags);
+	free(padding);
+	free(old_str);
+}
+
+static char	*get_padded_str(char **old_str, char *padding,
+				int is_negative, size_t *flags)
+{
+	char	*padded_str;
+
 	if (flags[NEGATIVE_WIDTH])
-		*str = ft_strjoin(old_str, width_adjustment);
+		padded_str = ft_strjoin(*old_str, padding);
 	else
-		*str = ft_strjoin(width_adjustment, old_str + is_negative);
+		padded_str = ft_strjoin(padding, *old_str + is_negative);
 	if (is_negative)
 	{
-		free(old_str);
-		old_str = *str;
-		*str = ft_strjoin("-", old_str);
+		free(*old_str);
+		*old_str = padded_str;
+		padded_str = ft_strjoin("-", *old_str);
 	}
-	free(width_adjustment);
-	free(old_str);
+	return (padded_str);
+}
+
+static int	check_if_is_negative(char *str, size_t *flags, char format)
+{
+	if (ft_strchr("di", format) && *str == '-' && flags[ZERO_PADDING])
+		return (1);
+	return (0);
 }
 
 static char	*get_padding(size_t padding_len, size_t *flags)
@@ -64,15 +77,15 @@ static char	*get_padding(size_t padding_len, size_t *flags)
 	return (padding);
 }
 
-static size_t	get_str_len(char **str, size_t *flags, char format)
+static size_t	get_str_len(char *str, size_t *flags, char format)
 {
 	size_t	len;
 
-	len = ft_strlen(*str);
+	len = ft_strlen(str);
 	if (format == 'c' && !len)
 		len = 1;
 	if (flags[PRECISION] && ft_strchr("di", format)
-		&& **str == '-' && flags[ZERO_PADDING])
+		&& *str == '-' && flags[ZERO_PADDING])
 		len--;
 	return (len);
 }
