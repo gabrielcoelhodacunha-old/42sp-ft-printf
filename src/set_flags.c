766f1859732ba7f929cf5a01498751dd2a5a7cd7
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-10-29 08:27:20 -0300

    Implemented flags '-0.' and minimum field width

--- src/set_flags.c
+++ src/set_flags.c
@@ -7,8 +7,7 @@ void	set_flags(char format, size_t *flags)
 	if ((!ft_strchr("di", format) && flags[BLANK])
 		|| (ft_strchr("di", format) && flags[SIGNED]))
 		flags[BLANK] = 0;
-	if (ft_strchr("diuxX", format) && flags[PRECISION_WIDTH])
-		flags[ZERO_PADDING] = 1;
-	if (flags[NEGATIVE_WIDTH])
+	if ((ft_strchr("diuxX", format) && flags[PRECISION])
+		|| (flags[NEGATIVE_WIDTH]))
 		flags[ZERO_PADDING] = 0;
 }
